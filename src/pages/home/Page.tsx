97d2c273ff2d68db7dc3b8c1319ea06b0dfe7760
Nenad Bursać <nbursa@gmail.com>
2023-05-06 19:53:01 +0200

    ga

--- src/pages/home/Page.tsx
+++ src/pages/home/Page.tsx
@@ -1,13 +1,28 @@
-import Link from "next/link";
 import PageContainer from "@/components/container/PageContainer";
+import {useEffect, useState} from "react";
+
+const Typing = ({text, duration = 1000}: { text: string; duration?: number }) => {
+  const [displayText, setDisplayText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const delay = 250;
+
+  useEffect(() => {
+    if (currentIndex < text.length) {
+      const timeoutId = setTimeout(() => {
+        setDisplayText((prevText) => prevText + text.charAt(currentIndex));
+        setCurrentIndex(currentIndex + 1);
+      }, delay);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [text, delay, currentIndex]);
+
+  return <div className="text-[12vw] text-white font-extrabold z-10 mt-[15vh]">{displayText}</div>;
+};
 
 const HomePage = () => {
   return (
-      <PageContainer title="Homepage" classNames="flex flex-col items-center justify-center">
-        <h1 className="-mt-40">Welcome to ML Studio</h1>
-        <Link href="/studio">
-          Go to ML Studio
-        </Link>
+      <PageContainer title="Homepage" classNames="flex flex-col items-center justify-start">
+        <Typing text="HI" duration={1000}/>
       </PageContainer>
   )
 }
