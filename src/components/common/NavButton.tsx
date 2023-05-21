c52085a961d9244f80df8f46cde8c863b777dc22
Nenad Bursać <nbursa@gmail.com>
2023-05-21 22:01:48 +0200

    nav update

--- src/components/common/NavButton.tsx
+++ src/components/common/NavButton.tsx
@@ -1,37 +1,12 @@
 import Link from "next/link";
-import {AlternateEmailOutlined, HomeSharp} from "@mui/icons-material";
 
 interface NavButtonProps {
   label: string;
   href: string;
+  icon: React.ReactNode;
 }
 
-const NavButton: React.FC<NavButtonProps> = ({href, label, ...props}) => {
-  const icon = (label: string) => {
-    if (label === "Contact") {
-      return (
-        // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2"
-        //      strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
-        //   <path
-        //     d="M21 10.59l-4.17-4.17a2 2 0 0 0-2.83 0L3 16.59a2 2 0 0 0 0 2.83l4.17 4.17a2 2 0 0 0 2.83 0L21 10.59a2 2 0 0 0 0-2.83z"></path>
-        //   <line x1="3" y1="21" x2="3.01" y2="21"></line>
-        // </svg>
-
-        <AlternateEmailOutlined stroke="#000000" fill="#000000" width="24" height="24"/>
-      );
-    } else if (label === "Home") {
-      return (
-        <HomeSharp color="secondary" width="24" height="24"/>
-        // <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#00008B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        //     <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2h-14a2 2 0 0 1-2-2z"></path>
-        //     <polyline points="9 22 9 12 15 12 15 22"></polyline>
-        // </svg>
-
-
-      );
-    }
-    return null;
-  };
+const NavButton: React.FC<NavButtonProps> = ({href, label, icon, ...props}) => {
 
   return (
     <Link {...props} href={href}
@@ -40,7 +15,7 @@ const NavButton: React.FC<NavButtonProps> = ({href, label, ...props}) => {
         className="flex items-center justify-center w-full h-full rounded-full bg-gray-light opacity-70 group-hover:opacity-100 transition-opacity duration-300 ease-in-out p-2"
       >
         <span className="group-hover:hidden">
-            {icon(label)}
+            {icon}
         </span>
         <span
           className="hidden text-sm font-bold text-transparent text-gray-dark group-hover:inline-block group-hover:text-gray-dark">
--- src/components/navigation/links.ts
+++
@@ -1,6 +0,0 @@
-import {NavLink} from "@/components/navigation/components/NavLinks";
-
-export const links: NavLink[] = [
-  {href: '/', label: 'Home'},
-  {href: '/contact', label: 'Contact'},
-]
