fea0783c3d9b4518481ec84e3c68c02715e8fbf8
Nenad Bursać <nbursa@gmail.com>
2023-09-08 13:10:30 +0200

    deployment script

--- src/components/Spotlight.tsx
+++ src/components/Spotlight.tsx
@@ -1,4 +1,5 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useRef} from "react";
+import styled from 'styled-components';
 
 type SpotlightProps = {
   id: number,
@@ -6,27 +7,34 @@ type SpotlightProps = {
   initialY: number,
   boundaryX: [number, number],
   boundaryY: [number, number],
-  ringColor: string
+  ringColor: string,
+  ringSize: number
 };
 
-const Spotlight: React.FC<SpotlightProps> = ({ initialX, initialY, boundaryX, boundaryY, ringColor, id }) => {
+type StyledSpotlightProps = {
+  ringColor: string;
+};
+
+const Spotlight: React.FC<SpotlightProps> = ({ initialX, initialY, boundaryX, boundaryY, ringColor, ringSize,  id }) => {
   const spotlightRef = useRef<HTMLDivElement>(null);
-  const circleRadius = (window.innerHeight / 100 * 50) / 2;
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+  const circleRadius = ringSize;
+  // const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+
+  const StyledSpotlight = styled.div<StyledSpotlightProps>`
+    width: 70vw;
+    height: 70vw;
+    border-radius: 50%;
+    position: absolute;
+    will-change: transform;
+    pointer-events: none;
+    left: 0;
+    top: 0;
+    background-color: ${(props) => props.ringColor};
+    opacity: 0.05;
+    filter: blur(100px);
+  `;
 
   const getRandomDirection = (): number => (Math.random() > 0.5 ? 1 : -1);
-
-  // useEffect(() => {
-  //   const handleMouseMove = (e: MouseEvent) => {
-  //     setMousePos({ x: e.clientX, y: e.clientY });
-  //   };
-  //
-  //   window.addEventListener("mousemove", handleMouseMove);
-  //
-  //   return () => {
-  //     window.removeEventListener("mousemove", handleMouseMove);
-  //   };
-  // }, []);
 
   useEffect(() => {
     let x = initialX - circleRadius;
@@ -37,14 +45,14 @@ const Spotlight: React.FC<SpotlightProps> = ({ initialX, initialY, boundaryX, bo
     const moveSpotlight = () => {
       if (spotlightRef.current === null) return;
 
-      const distanceToMouse = Math.sqrt(
-        Math.pow(x - mousePos.x, 2) + Math.pow(y - mousePos.y, 2)
-      );
+      // const distanceToMouse = Math.sqrt(
+      //   Math.pow(x - mousePos.x, 2) + Math.pow(y - mousePos.y, 2)
+      // );
 
-      if (distanceToMouse < 100) {
-        dx = -dx;
-        dy = -dy;
-      }
+      // if (distanceToMouse < 100) {
+      //   dx = -dx;
+      //   dy = -dy;
+      // }
 
       if (x < boundaryX[0] || x > boundaryX[1]) dx = -dx;
       if (y < boundaryY[0] || y > boundaryY[1]) dy = -dy;
@@ -57,23 +65,14 @@ const Spotlight: React.FC<SpotlightProps> = ({ initialX, initialY, boundaryX, bo
     };
 
     moveSpotlight();
-  }, [boundaryX, boundaryY, circleRadius, initialX, initialY, mousePos.x, mousePos.y]);
+  }, [boundaryX, boundaryY, circleRadius, initialX, initialY]);
 
   return (
-    <div
+    <StyledSpotlight
       id={`${id}`}
       ref={spotlightRef}
-      className="w-[70vw] h-[70vw] rounded-full absolute"
-      style={{
-        willChange: 'transform',
-        pointerEvents: 'none',
-        left: `0px`,
-        top: `0px`,
-        backgroundColor: ringColor,
-        opacity: '0.05',
-        filter: 'blur(100px)',
-      }}
-    ></div>
+      ringColor={ringColor}
+    ></StyledSpotlight>
   );
 };
 
