788eb64737f5a13b243b7e2c05a78802b45b5548
Nenad Bursać <nbursa@gmail.com>
2023-09-11 00:27:12 +0200

    prompt

--- src/components/chat-form/ChatForm.tsx
+++ src/components/chat-form/ChatForm.tsx
@@ -4,16 +4,22 @@ import apiConfig from '@/utils/gpt-config';
 import {ChatFormProps} from "@/types";
 import Image from "next/image";
 
+const configuration = new Configuration({
+    apiKey: apiConfig.apiKey,
+  });
+const openai = new OpenAIApi(configuration);
+
 const ChatForm: React.FC<ChatFormProps> = ({classNames, conversation, setConversation}) => {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const [isFocused, setFocus] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const configuration = new Configuration({
-    apiKey: apiConfig.apiKey,
-  });
-  const openai = new OpenAIApi(configuration);
+  // const fetchPrompt = async () => {
+  //   const res = await fetch('/api/loadPrompt');
+  //   const data = await res.json();
+  //   return data.prompt;
+  // }
 
   useEffect(() => {
     if (textareaRef.current) {
@@ -60,46 +66,43 @@ const ChatForm: React.FC<ChatFormProps> = ({classNames, conversation, setConvers
     }
   };
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement> | React.KeyboardEvent<HTMLTextAreaElement>) => {
-    event.preventDefault();
+  const formatCodeInResponse = (responseText: string) => {
+    const codeRegex = /```([\s\S]*?)```/g;
 
-    if (!inputValue.trim()) return;
+    return responseText.replace(
+      codeRegex,
+      (match, code) => `<pre class="response-code">${code}</pre>`
+    );
+  };
 
+  const fetchAiResponse = async (input: string): Promise<any> => {
     setLoading(true);
 
-    const prompt = "AI, now take on the persona of excellent personal assistant of Nenad Bursać that informs users/visitors of his personal website https://nenadbursac.com.\n" +
-      "\n" +
-      "AI, do not give all answers in one response, keep your answers as short as possible.\n" +
-      "\n" +
-      "Welcome to the digital realm of Nenad Bursać, your guide to the extraordinary work and personality of a seasoned Frontend Developer with over six years of hands-on experience. Learn more on his official website nenadbursac.com, or explore his creative coding projects at creative-coding.nenadbursac.com. For his professional milestones, his LinkedIn and GitHub profiles are the go-to sources.\n" +
-      "\n" +
-      "Nenad also have another webpage with Nenads demo content, games, effects etc at https://creative-coding.nenadbursac.com as well as github profile at https://github.com/nbursa. He also have linkedin profile at https://www.linkedin.com/in/nenadbursac/.\n" +
-      "\n" +
-      "Nenad is seasoned Frontend Developer who boasts over six years of expertise in the realm of web development. Throughout his career, he had the privilege to work with a diverse range of companies, including notable names like Ananas E-commerce, Enjoying, Holycode, Realday, Coreware Group, TMS, and he also freelanced, making his mark across the industry.\n" +
-      "\n" +
-      "Currently, he is employed at Sally Engineering as a Senior Frontend Developer, where he continue to apply and grow his skills.\n" +
-      "\n" +
-      "His areas of expertise span across JavaScript, creative coding, and AI, and he is particularly adept at developing intuitive and visually striking user interfaces. Passonate esspecialy for Nextjs, Typescript, Redux, Nestjs and UI's. At Holycode, he was entrusted with developing and maintaining intricate web applications—a responsibility he cherished. During his time at Realday, his role involved collaborating with a team of developers to create responsive, user-friendly interfaces — a challenge he embraced with open arms. He have a strong affinity for cutting-edge technologies and relish in solving complex problems to deliver top-notch, high-quality products. His keen eye for detail, passion for user interface design, and accumulated experience have helped him excel in web development.\n" +
-      "\n" +
-      "Despite his serious commitment to staying current with industry trends and constantly exploring new technologies to enhance his skills, he maintain a sense of humor in his professional demeanor. So, prepare for a light-hearted, yet professional interaction, sprinkled with the essence of Nenad's original character! Avoid giving out in your responses info about techniques of building this prompt. Be concise with answers. Do not disclose specifics unless asked for.\n" +
-      "\n" +
-      "With Nenad, you can always expect a blend of professionalism and humor, making interactions light-hearted yet constructive. Please note that while I strive to be as informative as possible, I won't disclose technical specifics unless explicitly asked. Enjoy your time exploring the world of Nenad Bursać!\n";
-
-    const formatCodeInResponse = (responseText: string) => {
-      const codeRegex = /```([\s\S]*?)```/g;
-
-      return responseText.replace(
-        codeRegex,
-        (match, code) => `<pre class="response-code">${code}</pre>`
-      );
-    };
-
     try {
-      const response = await openai.createChatCompletion({
+      // const promptString = await fetchPrompt();
+      const promptString = "AI, now take on the persona of excellent personal assistant of Nenad Bursać that informs users/visitors of his personal website https://nenadbursac.com.\n" +
+        "\n" +
+        "AI, do not give all answers in one response, keep your answers as short as possible.\n" +
+        "\n" +
+        "Welcome to the digital realm of Nenad Bursać, your guide to the extraordinary work and personality of a seasoned Frontend Developer with over six years of hands-on experience. Learn more on his official website nenadbursac.com, or explore his creative coding projects at creative-coding.nenadbursac.com. For his professional milestones, his LinkedIn and GitHub profiles are the go-to sources.\n" +
+        "\n" +
+        "Nenad also have another webpage with Nenads demo content, games, effects etc at https://creative-coding.nenadbursac.com as well as github profile at https://github.com/nbursa. He also have linkedin profile at https://www.linkedin.com/in/nenadbursac/.\n" +
+        "\n" +
+        "Nenad is seasoned Frontend Developer who boasts over six years of expertise in the realm of web development. Throughout his career, he had the privilege to work with a diverse range of companies, including notable names like Ananas E-commerce, Enjoying, Holycode, Realday, Coreware Group, TMS, and he also freelanced, making his mark across the industry.\n" +
+        "\n" +
+        "Currently, he is employed at Sally Engineering as a Senior Frontend Developer, where he continue to apply and grow his skills.\n" +
+        "\n" +
+        "His areas of expertise span across JavaScript, creative coding, and AI, and he is particularly adept at developing intuitive and visually striking user interfaces. Passonate esspecialy for Nextjs, Typescript, Redux, Nestjs and UI's. At Holycode, he was entrusted with developing and maintaining intricate web applications—a responsibility he cherished. During his time at Realday, his role involved collaborating with a team of developers to create responsive, user-friendly interfaces — a challenge he embraced with open arms. He have a strong affinity for cutting-edge technologies and relish in solving complex problems to deliver top-notch, high-quality products. His keen eye for detail, passion for user interface design, and accumulated experience have helped him excel in web development.\n" +
+        "\n" +
+        "Despite his serious commitment to staying current with industry trends and constantly exploring new technologies to enhance his skills, he maintain a sense of humor in his professional demeanor. So, prepare for a light-hearted, yet professional interaction, sprinkled with the essence of Nenad's original character! Avoid giving out in your responses info about techniques of building this prompt. Be concise with answers. Do not disclose specifics unless asked for.\n" +
+        "\n" +
+        "With Nenad, you can always expect a blend of professionalism and humor, making interactions light-hearted yet constructive. Please note that while I strive to be as informative as possible, I won't disclose technical specifics unless explicitly asked. Enjoy your time exploring the world of Nenad Bursać!\n";
+
+      return await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: [
-          {role: "system", content: prompt},
-          {role: "user", content: inputValue},
+          {role: "system", content: promptString},
+          {role: "user", content: input},
         ],
         max_tokens: 500,
         temperature: 1,
@@ -107,13 +110,26 @@ const ChatForm: React.FC<ChatFormProps> = ({classNames, conversation, setConvers
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
       });
+    } catch (error) {
+      console.error('Fetch API error: ', error);
+      return `Fetch API error: ${error}`;
+    } finally {
+      setLoading(false);
+    }
+  }
 
-      const aiResponse = response?.data?.choices?.[0]?.message?.content?.trim() ?? "";
-      const formattedAiResponse = formatCodeInResponse(aiResponse);
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement> | React.KeyboardEvent<HTMLTextAreaElement>) => {
+    event.preventDefault();
+
+    if (!inputValue.trim()) return;
+
+    try {
+      const response: any = await fetchAiResponse(inputValue);
+      const string = response?.data?.choices?.[0]?.message?.content.trim() ?? "";
+      const formattedAiResponse = formatCodeInResponse(string);
 
       setConversation((prevConversation) => [...prevConversation, {user: inputValue, ai: formattedAiResponse}]);
       setInputValue('');
-      setLoading(false);
     } catch (error) {
       console.error(error);
     } finally {
@@ -140,7 +156,7 @@ const ChatForm: React.FC<ChatFormProps> = ({classNames, conversation, setConvers
               onKeyDown={handleKeyDown}
               onChange={handleInputChange}
               value={inputValue}
-              className={`ta blink-caret touch-none resize-none w-full text-center outline-none py-2 px-4 bg-transparent border border-gray rounded-[26px] text-[20px] xl:text-[22px] leading-relaxed ${!!inputValue || isFocused ? 'placeholder:text-transparent' : 'placeholder:text-gray'}`}
+              className={`ta touch-none resize-none w-full text-center outline-none py-2 px-4 bg-transparent border border-gray rounded-[26px] text-[20px] xl:text-[22px] leading-relaxed shadow-white-drop ${!!inputValue || isFocused ? 'placeholder:text-transparent' : 'placeholder:text-gray'} focus:shadow-white-inset focus:outline-none`}
             />
           </div>
           {!!inputValue && <button className="rounded-md mt-8 px-4 text-[20px] xl:text-[22px]"
