ad6ea04994e31df999ae85ea640ada6ed355cda4
Nenad <nbursa@gmail.com>
2024-05-01 12:52:35 +0200

    pages

--- next.config.js
+++ next.config.js
@@ -2,10 +2,17 @@
 const nextConfig = {
     basePath: '',
     assetPrefix: '/',
+    env: {
+        NEXT_PUBLIC_CHATGPT_API_KEY: process.env.NEXT_PUBLIC_CHATGPT_API_KEY,
+        NEXT_PUBLIC_CHATGPT_API_URL: process.env.NEXT_PUBLIC_CHATGPT_API_URL,
+        PORT: process.env.PORT,
+        NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID,
+        EMAIL_USERNAME: process.env.EMAIL_USERNAME,
+        EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+        EMAIL_HOST: process.env.EMAIL_HOST,
+        EMAIL: process.env.EMAIL
+    },
     reactStrictMode: true,
-    // webpack(config) {
-    //     return config;
-    // },
     images: {
         domains: [],
         unoptimized: true
